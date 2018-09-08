072d747163a01dff7f3dc9540f13c808fafbbb14
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-09-08 10:29:08 +0100

    add method to remove single items based on last key stored in state

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { AsyncStorage } from "react-native"
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
+    keys:null
+  }
 
+  // removes single item from storage based on the keys in state
+  removeSingleItem = async () => {
+    const keys = this.state.keys;
+    if(keys.length !== 0){
+      let key = await keys.pop();
+      alert(`key is, ${key}`)
+      try{
+        await AsyncStorage.removeItem(key);
+      }
+      catch(error){
+        console.log(error)
+      }
+    }else{
+      alert("no more to remove!")
+    }
   }
 
   // save a single item in storage
   storeSingleItem = async () => {
-    try {
-      await AsyncStorage.setItem('KEY123456', 'I like to save it.');
-    } catch (error) {
-      // Error saving data
+    try{
+      await AsyncStorage.setItem("KEY34567890", "I'm a value");
+    }
+    catch(error){
+      console.log(error);
     }
   }
 
@@ -21,7 +39,8 @@ export default class App extends React.Component {
     try{
       const keys = await AsyncStorage.getAllKeys();
       if(keys !== null){
-        alert(JSON.stringify(keys));
+        this.setState(()=> ({keys}))
+        alert(JSON.stringify(this.state.keys));
       }
     }
     catch(error){
@@ -30,7 +49,7 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    this.storeSingleItem();
+    // this.storeSingleItem();
     this.retrieveKeys();
   }
 
@@ -38,6 +57,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button title="clear storage" onPress={this.removeSingleItem}/>
       </View>
     );
   }
